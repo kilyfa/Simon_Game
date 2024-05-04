7ff03e8f6e2ad9b841017c619707071d4770bd35
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-05-04 18:43:07 +0700

    completed(?)

--- game.js
+++ game.js
@@ -1,8 +1,80 @@
 const buttonColors = ["green", "red", "yellow", "blue"];
-const GamePattern = [];
+var GamePattern = [];
+var userClick = [];
+var start = false;
 
-function randomSequence(number) {
+function randomSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   RandomChosen = buttonColors[randomNumber];
   GamePattern.push(RandomChosen);
+}
+
+function playSound(text) {
+  const soundProgram = new Audio("sounds/" + text + ".mp3");
+  soundProgram.play();
+}
+
+function EqualAnswer(array1, array2) {
+  if (array1.length > 0) {
+    if (array1.length !== array2.length) {
+      return false;
+    }
+
+    for (var i = 0; i <= array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return WrongSide();
+      }
+    }
+    return setTimeout(function () {
+      GameArray();
+    }, 1000);
+  }
+}
+
+$(".btn").click(function () {
+  var click_id = $(this).attr("id");
+  userClick.push(click_id);
+  $("#" + click_id).addClass("pressed");
+  playSound(click_id);
+  setTimeout(function () {
+    $("#" + click_id).removeClass("pressed");
+  }, 100);
+  console.log(userClick);
+  //Logic
+  EqualAnswer(GamePattern, userClick);
+});
+
+//Khusus Program
+$(document).on("keypress", function () {
+  if (!start) {
+    userClick = [];
+    $("#level-title").text("Level " + GamePattern.length);
+    randomSequence();
+    $("#" + RandomChosen)
+      .fadeIn(100)
+      .fadeOut(100)
+      .fadeIn(100);
+    playSound(RandomChosen);
+    console.log(GamePattern);
+    start = true;
+  }
+});
+
+function GameArray() {
+  userClick = [];
+  $("#level-title").text("Level " + GamePattern.length);
+  randomSequence();
+  $("#" + RandomChosen)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(RandomChosen);
+  console.log(GamePattern);
+}
+
+function WrongSide() {
+  $("#level-title").text("Wrong!! TRY AGAIN!!");
+  playSound("wrong");
+  GamePattern = [];
+  start = false;
 }
