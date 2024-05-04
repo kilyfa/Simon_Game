6992c365198e2bd724c4a87672e3213eaaa9b380
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-05-04 10:37:25 +0700

    click_button

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 const buttonColors = ["green", "red", "yellow", "blue"];
+const GamePattern = [];
 
 function randomSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   RandomChosen = buttonColors[randomNumber];
+  GamePattern.push(RandomChosen);
 
   return RandomChosen;
 }
